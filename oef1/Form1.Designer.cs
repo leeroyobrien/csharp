1dd0f217f6e7fecb02bc37f3f77e105b8e97c7d4
leeroy obrien <leeroysport38@gmail.com>
2021-11-17 09:26:36 +0100

    eerste c_sharp project

---
+++ oef1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace oef1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtwords = new System.Windows.Forms.TextBox();
+            this.btnsort2 = new System.Windows.Forms.Button();
+            this.btnsort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtwords
+            // 
+            this.txtwords.Location = new System.Drawing.Point(12, 12);
+            this.txtwords.Multiline = true;
+            this.txtwords.Name = "txtwords";
+            this.txtwords.Size = new System.Drawing.Size(138, 140);
+            this.txtwords.TabIndex = 0;
+            this.txtwords.TextChanged += new System.EventHandler(this.Txtwords_TextChanged);
+            // 
+            // btnsort2
+            // 
+            this.btnsort2.Location = new System.Drawing.Point(174, 132);
+            this.btnsort2.Name = "btnsort2";
+            this.btnsort2.Size = new System.Drawing.Size(107, 20);
+            this.btnsort2.TabIndex = 1;
+            this.btnsort2.Text = "sort2";
+            this.btnsort2.UseVisualStyleBackColor = true;
+            this.btnsort2.Click += new System.EventHandler(this.Btnsort2_Click);
+            // 
+            // btnsort
+            // 
+            this.btnsort.Location = new System.Drawing.Point(174, 12);
+            this.btnsort.Name = "btnsort";
+            this.btnsort.Size = new System.Drawing.Size(107, 20);
+            this.btnsort.TabIndex = 2;
+            this.btnsort.Text = "sort";
+            this.btnsort.UseVisualStyleBackColor = true;
+            this.btnsort.Click += new System.EventHandler(this.Btnsort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsort);
+            this.Controls.Add(this.btnsort2);
+            this.Controls.Add(this.txtwords);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtwords;
+        private System.Windows.Forms.Button btnsort2;
+        private System.Windows.Forms.Button btnsort;
+    }
+}
+
---
+++ oef1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace oef1
+{
+    public partial class Form1 : Form
+    {
+        string[] words = new string[5];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i != words.Length; i++) 
+            {
+                words[i] = Interaction.InputBox("give 5 words", "5 words");
+                
+            }
+            for(int j =0; j!= words.Length;j++)
+            {
+                txtwords.Text += words[j] +Environment.NewLine;
+            }
+        }
+
+        private void Txtwords_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnsort_Click(object sender, EventArgs e)
+        {
+            txtwords.Text = "";
+
+            Array.Sort(words);
+            for (int k = 0; k != words.Length; k++)
+            {
+                txtwords.Text += words[k] + Environment.NewLine;
+            }
+            
+        }
+
+        private void Btnsort2_Click(object sender, EventArgs e)
+        {
+            txtwords.Text = "";
+            for (int i = 0; i <= 3; i++)
+            {
+                string hulp;
+                for (int j = i + 1; j <= 4; j++)
+                {
+                    if (words[i].CompareTo(words[j]) > 0)
+                    {
+                        hulp = words[i];
+                        words[i] = words[j];
+                        words[j] = hulp;
+                    }
+                }
+            }
+            for (int k = 0; k != words.Length; k++)
+            {
+                txtwords.Text += words[k] + Environment.NewLine;
+            }
+        }
+    }
+}
